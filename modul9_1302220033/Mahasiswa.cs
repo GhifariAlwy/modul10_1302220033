f05284e265f716c49650758ab8fad02e6459aeda
unknown <alwyghifari1@gmail.com>
2024-04-30 13:52:39 +0700

    menambahkan API Mahasiswa

--- modul9_1302220033/Mahasiswa.cs
+++ modul9_1302220033/Mahasiswa.cs
@@ -9,16 +9,18 @@ namespace WebApiMahasiswa
     {
         public string Nama { get; set; }
         public string Nim { get; set; }
+        public string Course { get; set; }
+        public string Year { get; set; }
     }
 
     public class MahasiswaController : ControllerBase
     {
         private static List<Mahasiswa> mahasiswaData = new List<Mahasiswa>()
         {
-            new Mahasiswa { Nama = "Ghifari", Nim = "1302220033" },
-            new Mahasiswa { Nama = "Alan", Nim = "1302229070" },
-            new Mahasiswa { Nama = "Aldino", Nim = "1302224344" },
-            new Mahasiswa { Nama = "Harits", Nim = "1302222656" },
+            new Mahasiswa { Nama = "Ghifari", Nim = "1302220033", Course = "KPL", Year = "2003" },
+            new Mahasiswa { Nama = "Alan", Nim = "1302229070", Course = "PBO", Year = "2004" },
+            new Mahasiswa {Nama = "Aldino", Nim = "1302224344", Course = "Basdat", Year = "2004"},
+            new Mahasiswa {Nama = "Harits", Nim = "1302222656", Course = "Jarkom", Year = "2003"},
         };
 
         [HttpGet("/api/mahasiswa")]
@@ -40,7 +42,7 @@ namespace WebApiMahasiswa
         [HttpPost("/api/mahasiswa")]
         public IActionResult AddMahasiswa([FromBody] Mahasiswa newMahasiswa)
         {
-            if (newMahasiswa == null || string.IsNullOrEmpty(newMahasiswa.Nama) || string.IsNullOrEmpty(newMahasiswa.Nim))
+            if (newMahasiswa == null || string.IsNullOrEmpty(newMahasiswa.Nama) || string.IsNullOrEmpty(newMahasiswa.Nim) || string.IsNullOrEmpty(newMahasiswa.Course) || string.IsNullOrEmpty(newMahasiswa.Year));
             {
                 return BadRequest("Nama dan NIM harus diisi");
             }
--- modul9_1302220033/obj/Debug/net8.0/modul9_1302220033.AssemblyInfo.cs
+++ modul9_1302220033/obj/Debug/net8.0/modul9_1302220033.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul9_1302220033")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b22df79e96e7c2c348ba8dd32bc9e4e13c2649d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b8fe68da60daf80190b02ea397c23b1e4c6814")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul9_1302220033")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul9_1302220033")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
